e0b4043e782b69c94b832fbef89ad45a7e926b0a
Umaima Yousuf <112200777+Umaima1205@users.noreply.github.com>
2023-09-19 19:46:44 +0500

    Add project files.

---
+++ if-else/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace myapp
+    {
+        internal class Program
+        {
+            static void Main(string[] args)
+            {
+                int Id = 32786532;
+                int AccountNumber = 002753809;
+                String Name = "Faiqa Yousuf";
+                int ConsumedUnit = 786;
+                int ConsumedUnitCost = 0;
+
+                if (ConsumedUnit <= 100)
+                {
+                    ConsumedUnitCost = 22;
+                }
+                else if (ConsumedUnit <= 200)
+                {
+                    ConsumedUnitCost = 32;
+                }
+                else if (ConsumedUnit <= 300)
+                {
+                    ConsumedUnitCost = 37;
+                }
+                else if (ConsumedUnit <= 400)
+                {
+                    ConsumedUnitCost = 43;
+                }
+                else if (ConsumedUnit <= 500)
+                {
+                    ConsumedUnitCost = 47;
+                }
+                else if (ConsumedUnit <= 600)
+                {
+                    ConsumedUnitCost = 49;
+                }
+                else if (ConsumedUnit <= 700)
+                {
+                    ConsumedUnitCost = 52;
+                }
+                else
+                {
+                    ConsumedUnitCost = 65;
+                }
+
+                Console.WriteLine("Id : " + Id);
+                Console.WriteLine("Name : " + Name);
+                Console.WriteLine("Account Number : " + AccountNumber);
+                Console.WriteLine("Consumed Unit : " + ConsumedUnit);
+                Console.WriteLine("Consumed Unit Cost : " + ConsumedUnitCost);
+
+                Console.ReadLine();
+            }
+        }
+    }
+   
